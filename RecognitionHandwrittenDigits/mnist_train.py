a3f44c3a8c62ac96cfd196e5a3e662c81e464537
hupengchen <hupengchen@rdc.gnway.com>
2023-12-04 23:12:02 +0800

    recognition handwrite digit

---
+++ RecognitionHandwrittenDigits/mnist_train.py
@@ -0,0 +1,120 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch import optim
+
+import torchvision
+from matplotlib import pyplot as plt
+from utils import plot_image, plot_curve, one_hot
+
+
+batch_size = 512
+
+# step1 load dataset
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('mnist_data',train=True, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (0.1307,), (0.3081,))
+                               ])),
+    batch_size=batch_size, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('mnist_data', train=False, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (0.1307,), (0.3081,))
+                               ])),
+    batch_size=batch_size, shuffle=False)
+
+x, y = next(iter(train_loader))
+print(x.shape, y.shape, x.min(), x.max())
+
+# plot_image(x, y, 'image sample')
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # wx+b
+        self.fc1 = nn.Linear(28*28, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        # 第一层
+        x = F.relu(self.fc1(x))
+        # 第二层
+        x = F.relu(self.fc2(x))
+        # 第三层
+        x = self.fc3(x)
+
+        return x
+
+net = Net()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+train_loss = []
+
+for epoch in range(3):
+
+    for batch_idx, (x, y) in enumerate(train_loader):
+
+        # x: [b, 1, 28, 28], y: [512]
+        x = x.view(x.size(0), 28*28)
+        out = net(x)
+        y_onehot = one_hot(y)
+        # loss = mse(out,y_onehot)
+        loss = F.mse_loss(out, y_onehot)
+
+        optimizer.zero_grad()
+        loss.backward()
+        # w' = w - lr*grad
+        optimizer.step()
+
+        train_loss.append(loss.item())
+
+        if batch_idx % 10 == 0:
+            print(epoch, batch_idx, loss.item())
+
+
+# plot_curve(train_loss)
+
+
+total_correct = 0
+for x, y in test_loader:
+    x = x.view(x.size(0), 28*28)
+    out = net(x)
+
+    # out:[b, 10]
+    pred = out.argmax(dim=1)
+    correct = pred.eq(y).sum().float().item()
+    total_correct += correct
+
+total_num = len(test_loader.dataset)
+acc = total_correct / total_num
+print('test acc:', acc)
+
+
+x, y = next(iter(test_loader))
+out = net(x.view(x.size(0), 28*28))
+pred = out.argmax(dim=1)
+plot_image(x, pred, 'test pred')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
